d2000f97a2a02da4f47ac46a962865661130387a
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-05-15 15:00:00 -0300

    fix: remove unused import

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
 
 export const Wrapper = styled.div`
   margin: 40px;
